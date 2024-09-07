c2c634895d34c9d0607810a9c9ab58c1591ab6aa
urie-w <urieasia@gmail.com>
2024-09-06 21:16:32 -0600

    Tokens verification added middleware files

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -20,5 +20,25 @@ router.post('/register', async (req, res) => {
 });
 
 //Login learner and generate a token
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        let learner = await User.findOne({ username });
+        if (!learner) return res.status(400).json({ message: 'Invalid login or password'});
 
+        //If password doesn't match
+        const matched = await learner.comparePassword(password);
+        if (!matched) return res.status(400).json({ message: 'Invalid login or password'});
+
+        //Id for learners
+        const token = jwt.sign(process.env.JWT_SECRET, { expiresIn: '1h'});
+        const learnerId = learner._id;
+
+        res.json({token});
+        } catch (err) {
+        res.status(500).json({ message: 'Server error'});
+    }
+});
+
+module.exports = router;
 
