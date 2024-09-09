0d2b0cafbede706b508cfa5c4d4e2e4ce10d15db
robel <robelabraha11@gmail.com>
2024-09-09 10:59:12 -0700

    updates on pages and utils folder

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to Uplearn! Here you can browse and enroll in new courses.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
