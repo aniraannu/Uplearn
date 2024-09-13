80f889f11a01f65fb0637707ec1f81135b2bfb80
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-09-12 18:53:08 -0700

    Changed the DB name

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 //Connection string
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/eplatformDB"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/edu_learn_DB"
 );
 //Export connection
 module.exports = mongoose.connection;
