8483ea340d619196db26a52cb33af0996656c3d0
urie-w <urieasia@gmail.com>
2024-09-15 02:11:42 -0600

    Finished JWT!

--- server/models/User.js
+++ server/models/User.js
@@ -16,16 +16,16 @@ const userSchema = new Schema({
 });
 
 // Hashing the password before saving the user
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  //Salt is making the password complex, it helps improve security.
-  const complex = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, complex);
+userSchema.pre("save", async function(next) {
+  if (this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
   next();
 });
 
 //Method to compare the password for login
-userSchema.methods.comparePassword = function (password) {
+userSchema.methods.isCorrectPassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
