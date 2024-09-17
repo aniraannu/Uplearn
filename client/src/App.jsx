c136525187b87dbd23a1f97781f99c272a95a9e0
Jose Arambula <jgrambula@gmail.com>
2024-09-16 21:38:02 -0700

    update to ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,113 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Box, Flex, Heading, Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Flex, Image, Link as ChakraLink } from '@chakra-ui/react';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import ManageCourses from './pages/ManageCourses';
-import Login from './pages/Login';   // Import Login component
-import Signup from './pages/Signup'; // Import Signup component
-
-
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
       <Box>
-        <Flex as="header" p={4} bg="teal.500" color="white" justifyContent="space-between" alignItems="center">
-          <Heading size="lg">UpLearn</Heading>
-          <Flex as="nav">
-            <ChakraLink as={Link} to="/" px={4}>Home</ChakraLink>
-            <ChakraLink as={Link} to="/dashboard" px={4}>Dashboard</ChakraLink>
-            <ChakraLink as={Link} to="/login" px={4}>Login</ChakraLink>    {/* Add Login link */}
-            <ChakraLink as={Link} to="/signup" px={4}>Signup</ChakraLink>  {/* Add Signup link */}
+        <Flex
+          as="header"
+          p={1} 
+          bg="white"          
+          color="orange.500"  
+          alignItems="center"
+          borderBottom="1px"  
+          borderColor="gray.200" 
+          h="60px"             
+        >
+          <Image
+            src="/images/logo.png"  
+            alt="UpLearn Logo"
+            boxSize="80px"           
+            objectFit="contain"      
+            borderRadius="full"      
+            _hover={{ transform: 'scale(1.1)', transition: 'transform 0.3s ease' }} 
+          />
+
+          <Flex
+            as="nav"
+            ml="auto" 
+            alignItems="center"
+            gap={3}   
+          >
+            <ChakraLink
+              as={Link}
+              to="/"
+              px={2} 
+              py={1}
+              fontSize="sm" 
+              borderRadius="md"      
+              _hover={{ 
+                bg: 'orange.100',    
+                color: 'orange.700',  
+                textDecoration: 'underline',
+                transition: 'all 0.3s ease'
+              }}
+            >
+              Home
+            </ChakraLink>
+            <ChakraLink
+              as={Link}
+              to="/dashboard"
+              px={2} 
+              py={1}
+              fontSize="sm" 
+              borderRadius="md"      
+              _hover={{ 
+                bg: 'orange.100',    
+                color: 'orange.700',  
+                textDecoration: 'underline',
+                transition: 'all 0.3s ease'
+              }}
+            >
+              Dashboard
+            </ChakraLink>
+            <ChakraLink
+              as={Link}
+              to="/login"
+              px={2} 
+              py={1}
+              fontSize="sm" 
+              borderRadius="md"      
+              _hover={{ 
+                bg: 'orange.100',    
+                color: 'orange.700',  
+                textDecoration: 'underline',
+                transition: 'all 0.3s ease'
+              }}
+            >
+              Login
+            </ChakraLink>
+            <ChakraLink
+              as={Link}
+              to="/signup"
+              px={2} 
+              py={1}
+              fontSize="sm" 
+              borderRadius="md"      
+              _hover={{ 
+                bg: 'orange.100',    
+                color: 'orange.700',  
+                textDecoration: 'underline',
+                transition: 'all 0.3s ease'
+              }}
+            >
+              Signup
+            </ChakraLink>
           </Flex>
         </Flex>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/manage-courses" element={<ManageCourses />} />
-          <Route path="/login" element={<Login />} />      {/* Add Login route */}
-          <Route path="/signup" element={<Signup />} />    {/* Add Signup route */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Box>
     </Router>
@@ -35,70 +115,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import { Box, Flex, Heading, Link as ChakraLink } from '@chakra-ui/react';
-// import Home from './pages/Home';
-// import Dashboard from './pages/Dashboard';
-// import ManageCourses from './pages/ManageCourses';
-// import Login from './pages/Login'; // Import the Login page
-
-// function App() {
-//   return (
-//     <Router>
-//       <Box>
-//         <Flex as="header" p={4} bg="teal.500" color="white" justifyContent="space-between" alignItems="center">
-//           <Heading size="lg">UpLearn</Heading>
-//           <Flex as="nav" alignItems="center">
-//             <ChakraLink as={Link} to="/" px={4}>Home</ChakraLink>
-//             <ChakraLink as={Link} to="/dashboard" px={4}>Dashboard</ChakraLink>
-//             <ChakraLink as={Link} to="/login" px={4}>Login</ChakraLink> {/* Added Login link */}
-//           </Flex>
-//         </Flex>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="/manage-courses" element={<ManageCourses />} />
-//           <Route path="/login" element={<Login />} /> {/* Added Login route */}
-//         </Routes>
-//       </Box>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-// // import React from 'react';
-// // import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// // import { Box, Flex, Heading, Link as ChakraLink } from '@chakra-ui/react';
-// // import Home from './pages/Home';
-// // import Dashboard from './pages/Dashboard';
-// // import ManageCourses from './pages/ManageCourses';
-
-// // function App() {
-// //   return (
-// //     <Router>
-// //       <Box>
-// //         <Flex as="header" p={4} bg="teal.500" color="white" justifyContent="space-between" alignItems="center">
-// //           <Heading size="lg">UpLearn</Heading>
-// //           <Flex as="nav">
-// //             <ChakraLink as={Link} to="/" px={4}>Home</ChakraLink>
-// //             <ChakraLink as={Link} to="/dashboard" px={4}>Dashboard</ChakraLink>
-// //           </Flex>
-// //         </Flex>
-// //         <Routes>
-// //           <Route path="/" element={<Home />} />
-// //           <Route path="/dashboard" element={<Dashboard />} />
-// //           <Route path="/manage-courses" element={<ManageCourses />} />
-// //         </Routes>
-// //       </Box>
-// //     </Router>
-// //   );
-// // }
-
-// // export default App;
-
